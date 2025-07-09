bb859d7c9ce89ffff8ce8bdab48ed38e370f32a7
marcosf7x <marcos.felipe1088@gmail.com>
2025-07-09 10:19:51 -0300

    Update code ex024

--- ex024 (Verificando as primeiras letras de um texto).py
+++ ex024 (Verificando as primeiras letras de um texto).py
@@ -1,3 +1,6 @@
-cidade = str(input("Em que cidade você nasceu? "))
-
-
+cidade = str(input("Em que cidade você nasceu? ")).strip().lower().title()
+# cidade_2 = cidade.strip().lower().title()
+analise = "Santo" in cidade
+# print(f"Esse foi o nome da cidade que você digitou: {cidade} o seu número de caracteres {len(cidade)}")
+print(f"{analise} e a quantidade de caracteres atualizado {len(cidade)} e o nome atualizado {cidade}")
+# print("analise")
